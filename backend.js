0db3d870cf033e16ad3419683c8f3a8e6f720cdf
Mark <mark_chaitra@hotmail.com>
2026-07-29 23:12:47 -0400

    figuring out letter check logic

--- backend.js
+++ backend.js
@@ -1,4 +1,4 @@
-let word ="moron";
+let word ="whore";
 let row = 1;
 let guess = '';
 
@@ -33,13 +33,13 @@ function wordCheck(box){
 
     let repeats = '';
 
+    
     // Goes through each letter in the row (5 letters)
     box.forEach((element, index) => {
 
-        console.log(index, "repeats",repeats.split(guess[index]).length -1, word.toUpperCase().split(guess[index]).length -1);
-        console.log(index, guess.split(guess[index]).length -1, word.toUpperCase().split(guess[index]).length -1);
+        repeats = repeats + guess[index];
 
-        //How to check if letter appears somewhere else??????
+        console.log("repeats", `index: ${index}`, repeats.split(guess[index]).length -1 <= word.toUpperCase().split(guess[index]).length -1, repeats.split(guess[index]).length -1, word.toUpperCase().split(guess[index]).length -1);
 
         // Shrink animation for background colours to change
         setTimeout(() => {
@@ -50,7 +50,7 @@ function wordCheck(box){
 
         // Letters match at the indices background green
         if(element.innerHTML.toUpperCase() == word[index].toUpperCase()){
-            repeats = repeats + guess[index];
+            // repeats = repeats + guess[index];
             setTimeout(() => {
                 element.style.backgroundColor = 'green';
                 element.style.border = '2px solid green';
@@ -58,8 +58,8 @@ function wordCheck(box){
         } 
 
         // Check if word includes letter, letter is not in repeats list more than in the actual word, and letter in guessed word appears less then in actual word
-        else if(word.toUpperCase().includes(guess[index].toUpperCase()) && repeats.split(guess[index]).length -1 < word.toUpperCase().split(guess[index]).length -1){
-            repeats = repeats + guess[index];
+        else if(word.toUpperCase().includes(guess[index].toUpperCase()) && repeats.split(guess[index]).length -1 <= word.toUpperCase().split(guess[index]).length -1 && (guess.split(guess[index]).length -1)-(index) <= word.toUpperCase().split(guess[index]).length -1){
+            // repeats = repeats + guess[index];
             setTimeout(() => {
                 element.style.backgroundColor = 'orange';
                 element.style.border = '2px solid orange';
